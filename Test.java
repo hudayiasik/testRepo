e5587bebefa3582cc239995dce41f9c004c6881a
hudayiasik <hudayi.asik@stu.fsm.edu.tr>
2024-04-14 12:08:51 +0300

    make it longer

--- Test.java
+++ Test.java
@@ -1,5 +1,138 @@
 public class Test{
     public static void main(String[] args) {
+        /*
+         * 
+         * 
+         * 
+         * 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+         ds
+
+
+         ds
+         ds
+
+         s
+         d
+         sd
+
+         sdd
+         s
+         d
+         sd
+
+         */
         System.out.println("hi");
+    }
+    public void x(){
+
+        int a;
+        /*
+         * 
+         * 
+         * 
+         * 
+         * 
+         * 
+         * 
+         * 
+         * 
+         * 
+         * 
+         * 
+         * 
+         * 
+         * 
+         * 
+         * 
+         * 
+         * 
+         * 
+         * dsds
+         * 
+         * 
+         * 
+         * 
+         * 
+         * sd
+         * 
+         * 
+         * 
+         * 
+         * 
+         * 
+         * 
+         * 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+         dsd
+
+         
+         */
     }
 }
