26de3024f859b298a3fea96132465b9341130b14
hudayiasik <hudayi.asik@stu.fsm.edu.tr>
2024-04-14 12:09:33 +0300

    2 line chaned

--- Test.java
+++ Test.java
@@ -2,7 +2,7 @@ public class Test{
     public static void main(String[] args) {
         /*
          * 
-         * 
+         ----- 
          * 
          * 
 
@@ -130,9 +130,9 @@ public class Test{
 
 
 
-         dsd
+         abc
 
-         
+
          */
     }
 }
